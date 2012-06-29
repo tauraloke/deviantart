2c8933ce28a77114c5b89b98a5bb6d2a35774e97
Tauraloke Esteru <tauraloke@gmail.com>
2012-06-29 23:40:11 +0400

    rest_client....

--- lib/deviantart.rb
+++ lib/deviantart.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'open-uri'
 require 'json'
 require 'yaml'
-require 'mechanize'
+require 'rest_client'
+
 
 
 class DeviantClient
@@ -60,16 +61,19 @@ class DeviantClient
     end
   end
   
-  def post resource, params={}
+  def post resource, options={}, filepath=nil
     url = API_PATH + resource + '?access_token=' + @access_token
-    browser = Mechanize.new
+    url = url + '&' + options.map{ |k,v| "#{k}="+URI::encode(v) }.join('&') if options
     begin
-      page = browser.post(url, params)
-      return JSON.parse(page.body)['available_space']
-    rescue
-		raise 'Trying post data by fail'
+      response = RestClient.post url, filepath ? { :file => File.new(filepath, 'rb') } : nil
+      return JSON.parse(response.to_str)
+    rescue Exception=>e
+		pp e
+		raise 'Trying post data but fail'
     end
   end
+  
+  def 
   
 
   def placebo
@@ -86,8 +90,19 @@ class DeviantClient
   end
   
   def stash_space
-    post 'stash/space'
+    data = post 'stash/space'
+    return data['available_space']
   end
-
+  
+  def submit params
+    option_keys = [:title, :artist_comments, :keywords, :stashid, :folder, :folderid]
+    options = {  }
+    option_keys.each do |key|
+	  options[key] = params[key] if params[key]
+    end
+    data = post 'stash/submit', options, params[:file]
+    data['link'] = 'http://sta.sh/0'+data['stashid'].to_i.to_s(36)
+    return data
+  end
 
 end
